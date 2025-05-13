777f33d6d09e25736e582e3ac139a4ce8efc7fd5
Francis Frias <francisr.frias@gmail.com>
2025-05-13 18:09:07 +0800

    push

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,6 @@
 import CustomPagination from '@/components/shared/custom-pagination';
+import CustomTableSkeleton from '@/components/shared/custom-table-skeleton';
+import { Suspense } from 'react';
 import ContentAcquisitionTable from './_components/table';
 import { TableSelection } from './_components/table-selection';
 
@@ -9,7 +11,7 @@ interface IProps {
 const MainPage = async ({ searchParams }: IProps) => {
   const { page } = await searchParams;
   const currentPage = Number(page) || 1;
-  const pageSize = 4;
+  const pageSize = 8;
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/crawl`, {
     method: 'GET',
@@ -26,7 +28,9 @@ const MainPage = async ({ searchParams }: IProps) => {
         <div className='bg-white border   '>
           <TableSelection data={data} />
         </div>
-        <ContentAcquisitionTable data={data} />
+        <Suspense fallback={<CustomTableSkeleton columns={4} length={5} />}>
+          <ContentAcquisitionTable data={data} />
+        </Suspense>
         <div className='p-2 border-t bg-gray-50 text-xs text-muted-foreground'>
           {data.length} Total Length
         </div>
