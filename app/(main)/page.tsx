4b5e29834a43e384456a44bfe446affaf8a0edfa
Francis Frias <francisr.frias@gmail.com>
2025-05-13 16:58:36 +0800

    public url added for fetching

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,7 +11,7 @@ const MainPage = async ({ searchParams }: IProps) => {
   const currentPage = Number(page) || 1;
   const pageSize = 4;
 
-  const res = await fetch('http://localhost:3000/api/crawl', {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/crawl`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
