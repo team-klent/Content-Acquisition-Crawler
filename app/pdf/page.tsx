37d6748a629dd2925051e937a629c7b12a83dd82
Kyle <kyle@payo.asia>
2025-05-17 00:23:39 +0800

    Add suspense

--- app/pdf/page.tsx
+++ app/pdf/page.tsx
@@ -1,9 +1,12 @@
 import PdfView from '@/components/organisms/components/pdf-view';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <main className="p-6">
-      <PdfView />
+      <Suspense fallback={<p>Loading PDF viewer...</p>}>
+        <PdfView />
+      </Suspense>
     </main>
   );
 }
