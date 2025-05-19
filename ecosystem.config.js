a660a9f8eceda24cac7b4194a367df4bdc754f10
Francis Frias <francisr.frias@gmail.com>
2025-05-19 12:23:45 +0800

    base path to false

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        USE_BASE_PATH: 'true',
+        USE_BASE_PATH: 'false',
         NEXT_PUBLIC_URL: 'https://unifiedworkflow.innodata.com/app1',
         FAST_REFRESH: 'false',
       },
