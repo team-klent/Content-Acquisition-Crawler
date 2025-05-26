861cad94143c700f08693b79c823bb6061e1f984
Francis Frias <francisr.frias@gmail.com>
2025-05-26 14:29:31 +0800

    fix id error

--- app/(main)/pdf/page.tsx
+++ app/(main)/pdf/page.tsx
@@ -13,6 +13,9 @@ export default async function Home({
   }
 
   const pdf = {
+    id:
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15),
     file_name: params.file_name as string,
     file_path: filePath,
     created_at: params.created_at as string,
