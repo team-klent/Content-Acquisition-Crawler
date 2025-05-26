06c67e662a85c8c6215702e0ba3a8133403f6927
Francis Frias <francisr.frias@gmail.com>
2025-05-26 15:16:51 +0800

    small fixes on image

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import UWLogo from '@/public/logo-unified-workflow.jpg';
+import Image from 'next/image';
+
 export default function MainPageLayout({
   children,
 }: Readonly<{
@@ -5,10 +8,17 @@ export default function MainPageLayout({
 }>) {
   return (
     <>
-      <nav className="bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div className="flex shrink-0 items-center p-[10px] py-[2px]">
-            <img className="h-[40px] w-auto" src="logo-unified-workflow.jpg" alt="Innodata" />
+      <nav className='bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700'>
+        <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+          <div className='flex shrink-0 items-center p-[10px] py-[2px]'>
+            <Image
+              className='h-[40px] w-auto'
+              src={UWLogo}
+              alt='Innodata'
+              width={160}
+              height={40}
+              priority
+            />
           </div>
         </div>
       </nav>
