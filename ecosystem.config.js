1db2c82f84c457e96149063ba11db054d28131ac
aritchelle <calaritchelle@gmail.com>
2025-05-27 16:39:40 +0800

    Refactor environment configuration and improve basePath handling in ecosystem and next config files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,25 @@ module.exports = {
       name: "content-acquisition-crawler",
       script: "npm",
       args: "start",
+      env_development: {
+        NODE_ENV: "development",
+        PORT: 3000,
+        USE_BASE_PATH: "false",
+        FAST_REFRESH: true
+      },
       env_production: {
         NODE_ENV: "production",
         PORT: 3000,
-        USE_BASE_PATH: true,
+        USE_BASE_PATH: "true",
         BASE_PATH: "/app1",
         NEXT_PUBLIC_URL: "https://unifiedworkflow.innodata.com/app1",
         FAST_REFRESH: false
       },
       watch: false,
       instances: 1,
-      autorestart: true
+      exec_mode: "fork",
+      autorestart: true,
+      exp_backoff_restart_delay: 100
     }
   ]
 };
