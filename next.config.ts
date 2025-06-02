3c5eb49c102d60d8bfb5d6000901365bf4f0ab4e
Francis Frias <francisr.frias@gmail.com>
2025-06-02 16:33:59 +0800

    sample fix

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,11 @@ const nextConfig: NextConfig = {
     process.env.USE_BASE_PATH === 'true'
       ? process.env.BASE_PATH || '/app1'
       : '',
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
+ webpack: (config) => {
+   config.resolve.alias.canvas = false;
+
+   return config;
+ },
 };
 
 export default nextConfig;
