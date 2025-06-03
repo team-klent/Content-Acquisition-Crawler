3c8b8a6c381777e4ffa64c1f4b1bf2ed39c38b05
Francis Frias <francisr.frias@gmail.com>
2025-06-03 22:48:55 +0800

    Header fix cant see image

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,11 +1,10 @@
-import DarkMainLogo from '@/public/images/MainLogo_DarkMode_Transparent.png';
 import Image from 'next/image';
 
 const Header = ({ children }: Readonly<{ children?: React.ReactNode }>) => {
   return (
-    <div className='max-w-full bg-[#000000] px-[1.55rem] py-[0.20rem] flex flex-row'>
+    <div className='max-w-full bg-[#000000] px-[1.55rem] py-[0.275rem] flex flex-row'>
       <Image
-        src={DarkMainLogo}
+        src='/images/MainLogo_DarkMode_Transparent.png'
         alt='Innodata Main Logo'
         width={176}
         height={40}
